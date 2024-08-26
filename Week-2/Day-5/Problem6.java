52ad2712238fc6c8319eb8bbc9bfd69140c99c39
PARANDHAMAREDDYBOMMAKA <rparandhama63@gmail.com>
2024-08-26 14:07:40 +0530

    Added Week-2 Day-5 Problems

---
+++ Week-2/Day-5/Problem6.java
@@ -0,0 +1,51 @@
+// For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+// Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ 
+
+// Example 1:
+
+// Input: str1 = "ABCABC", str2 = "ABC"
+// Output: "ABC"
+// Example 2:
+
+// Input: str1 = "ABABAB", str2 = "ABAB"
+// Output: "AB"
+// Example 3:
+
+// Input: str1 = "LEET", str2 = "CODE"
+// Output: ""
+ 
+
+// Constraints:
+
+// 1 <= str1.length, str2.length <= 1000
+// str1 and str2 consist of English uppercase letters.
+
+// Solution
+
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        int len1 = str1.length();
+        int len2 = str2.length();
+        
+        int gcd = gcd(len1, len2);
+        String gcdStr = str1.substring(0, gcd);
+        
+        if (str1.equals(gcdStr.repeat(len1 / gcd)) && str2.equals(gcdStr.repeat(len2 / gcd))) {
+            return gcdStr;
+        }
+        
+        return "";
+    }
+    
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
